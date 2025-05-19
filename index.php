0f5703849b0338ecd529074a0a379fd29431e95a
JesseHardorff <144345868+JesseHardorff@users.noreply.github.com>
2025-05-19 09:37:09 +0200

    layout index

--- index.php
+++ index.php
@@ -24,22 +24,23 @@
     </script>
 </head>
 
-<body class="bg-light text-center p-10">
-    <h1 class="text-3xl font-bold text-primary">Hallo vanaf index.php!</h1>
+<body class="bg-primary text-center p-0 ">
+    <header class="h-[15vh] w-full bg-light">
 
-    <div class="max-w-md mx-auto mt-8 p-6 bg-white rounded-lg shadow-md">
-        <h2 class="text-2xl font-semibold text-dark mb-4">Aangepaste kleuren demo</h2>
+    </header>
+    <div class="h-[70vh] w-full bg-secondary">
+    </div>
+    <footer class="h-[15vh] w-full bg-base">
 
-        <div class="grid grid-cols-1 gap-4">
+    </footer>
+</body>
+
+<!-- <div class="grid grid-cols-1 gap-4">
             <div class="p-4 bg-primary text-white rounded">Primary kleur</div>
             <div class="p-4 bg-secondary text-white rounded">Secondary kleur</div>
             <div class="p-4 bg-accent text-white rounded">Accent kleur</div>
             <div class="p-4 bg-dark text-white rounded">Dark kleur</div>
             <div class="p-4 bg-light text-dark rounded border">Light kleur</div>
-        </div>
-
-
-    </div>
-</body>
+        </div> -->
 
 </html>
